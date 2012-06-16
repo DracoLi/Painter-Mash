b7602878636176c9d018054e70bd0505c324b7d4
Draco Li <draco@dracoli.com>
2012-06-16 13:17:07 -0400

    wallpapers

--- coffee/paintings.coffee
+++ coffee/paintings.coffee
@@ -1,24 +1,27 @@
 $ ->
   
   class Paintings
-    paintings: []
-    add_panting: (icon, description) ->
+    free_paintings: []
+    paid_paintings: []
+    add_free: (icon, description) ->
       paint = {icon: icon, description: description}
-      @paintings.push paint
+      @free_paintings.push paint
+    add_paid: (icon, description, price) ->
+      paint = {icon: icon, description: description, price: price}
+      @paid_paintings.push paint
   paintings = new Paintings
-  paintings.add_panting 'miro', 'micro finance'
-  paintings.add_panting 'magritte', 'smock alot'
-  paintings.add_panting 'mondrian', 'mandarin'
-  paintings.add_panting 'rothko', 'rock is cool'
-  
+  paintings.add_free 'miro', 'micro finance'
+  paintings.add_free 'magritte', 'smock alot'
+  paintings.add_paid 'mondrian', 'mandarin', 3000
+  paintings.add_paid 'rothko', 'rock is cool', 5000
   
   class PaintingManager
   
     free_selector: $('#free_paintings .paintings')
     
-    $paid_selector: $('#paid_paintings .paintings')
+    paid_selector: $('#paid_paintings .paintings')
   
-    paint_html: """
+    free_html: """
       <div class="paint">
         <div class="paint_image">
           <img src="" />
@@ -30,16 +33,38 @@ $ ->
         <button>Use This</button>
       </div>
     """
-  
+    paid_html: """
+      <div class="paint">
+        <div class="paint_image">
+          <img src="" />
+        </div>
+        <div class="paint_description">
+          
+        </div>
+        <div class="paint_price"></div>
+        <button>Use This</button>
+      </div>
+    """
     add_free: (paint) ->
-      {description, icon} = paint
-      new_paint = $(@paint_html).clone()
-      $(new_paint).find('.paint_description').html description
-      $(new_paint).find('.paint_image img').attr 'src', "resources/#{icon}_icon.png"
+      new_paint = @add_general paint, @free_html
       @free_selector.append new_paint
       console.log new_paint
-    
+    add_paid: (paint) ->
+      console.log paint.price
+      new_paint = @add_general paint, @paid_html
+      $(new_paint).find('.paint_price').html paint.price
+      @paid_selector.append new_paint
+      console.log new_paint
+    add_general: (paint, html) ->
+      {description, icon} = paint
+      new_paint = $(html).clone()
+      $(new_paint).find('.paint_description').html description
+      $(new_paint).find('.paint_image img').attr 'src', "resources/#{icon}_icon.png"
+      new_paint
+      
   paintManager = new PaintingManager
 
-  for paint in paintings.paintings
+  for paint in paintings.free_paintings
     paintManager.add_free paint
+  for paint in paintings.paid_paintings
+    paintManager.add_paid paint
--- src/js/paintings.js
+++ src/js/paintings.js
@@ -1,59 +1,93 @@
 (function() {
 
   $(function() {
-    var PaintingManager, Paintings, paint, paintManager, paintings, _i, _len, _ref, _results;
+    var PaintingManager, Paintings, paint, paintManager, paintings, _i, _j, _len, _len2, _ref, _ref2, _results;
     Paintings = (function() {
 
       function Paintings() {}
 
-      Paintings.prototype.paintings = [];
+      Paintings.prototype.free_paintings = [];
 
-      Paintings.prototype.add_panting = function(icon, description) {
+      Paintings.prototype.paid_paintings = [];
+
+      Paintings.prototype.add_free = function(icon, description) {
         var paint;
         paint = {
           icon: icon,
           description: description
         };
-        return this.paintings.push(paint);
+        return this.free_paintings.push(paint);
+      };
+
+      Paintings.prototype.add_paid = function(icon, description, price) {
+        var paint;
+        paint = {
+          icon: icon,
+          description: description,
+          price: price
+        };
+        return this.paid_paintings.push(paint);
       };
 
       return Paintings;
 
     })();
     paintings = new Paintings;
-    paintings.add_panting('miro', 'micro finance');
-    paintings.add_panting('magritte', 'smock alot');
-    paintings.add_panting('mondrian', 'mandarin');
-    paintings.add_panting('rothko', 'rock is cool');
+    paintings.add_free('miro', 'micro finance');
+    paintings.add_free('magritte', 'smock alot');
+    paintings.add_paid('mondrian', 'mandarin', 3000);
+    paintings.add_paid('rothko', 'rock is cool', 5000);
     PaintingManager = (function() {
 
       function PaintingManager() {}
 
       PaintingManager.prototype.free_selector = $('#free_paintings .paintings');
 
-      PaintingManager.prototype.$paid_selector = $('#paid_paintings .paintings');
+      PaintingManager.prototype.paid_selector = $('#paid_paintings .paintings');
 
-      PaintingManager.prototype.paint_html = "<div class=\"paint\">\n  <div class=\"paint_image\">\n    <img src=\"\" />\n  </div>\n  <div class=\"paint_description\">\n  \n  </div>\n\n  <button>Use This</button>\n</div>";
+      PaintingManager.prototype.free_html = "<div class=\"paint\">\n  <div class=\"paint_image\">\n    <img src=\"\" />\n  </div>\n  <div class=\"paint_description\">\n  \n  </div>\n\n  <button>Use This</button>\n</div>";
+
+      PaintingManager.prototype.paid_html = "<div class=\"paint\">\n  <div class=\"paint_image\">\n    <img src=\"\" />\n  </div>\n  <div class=\"paint_description\">\n    \n  </div>\n  <div class=\"paint_price\"></div>\n  <button>Use This</button>\n</div>";
 
       PaintingManager.prototype.add_free = function(paint) {
-        var description, icon, new_paint;
-        description = paint.description, icon = paint.icon;
-        new_paint = $(this.paint_html).clone();
-        $(new_paint).find('.paint_description').html(description);
-        $(new_paint).find('.paint_image img').attr('src', "resources/" + icon + "_icon.png");
+        var new_paint;
+        new_paint = this.add_general(paint, this.free_html);
         this.free_selector.append(new_paint);
         return console.log(new_paint);
+      };
+
+      PaintingManager.prototype.add_paid = function(paint) {
+        var new_paint;
+        console.log(paint.price);
+        new_paint = this.add_general(paint, this.paid_html);
+        $(new_paint).find('.paint_price').html(paint.price);
+        this.paid_selector.append(new_paint);
+        return console.log(new_paint);
+      };
+
+      PaintingManager.prototype.add_general = function(paint, html) {
+        var description, icon, new_paint;
+        description = paint.description, icon = paint.icon;
+        new_paint = $(html).clone();
+        $(new_paint).find('.paint_description').html(description);
+        $(new_paint).find('.paint_image img').attr('src', "resources/" + icon + "_icon.png");
+        return new_paint;
       };
 
       return PaintingManager;
 
     })();
     paintManager = new PaintingManager;
-    _ref = paintings.paintings;
-    _results = [];
+    _ref = paintings.free_paintings;
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       paint = _ref[_i];
-      _results.push(paintManager.add_free(paint));
+      paintManager.add_free(paint);
+    }
+    _ref2 = paintings.paid_paintings;
+    _results = [];
+    for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+      paint = _ref2[_j];
+      _results.push(paintManager.add_paid(paint));
     }
     return _results;
   });
